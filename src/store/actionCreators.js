e18e85bc6f4e7b33226167c12f83f4f75a85d6d4
heyushuo <1007561607@qq.com>
2018-09-13 14:11:22 +0800

    redux-thunk

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -2,3 +2,21 @@ export const getInputChange = (value) => ({
     type: '',
     value
 })
+
+
+export const getInitData = () => {
+    //可以接收dispatch
+    return (dispatch) => {
+        this.axios.get('/list.data', {
+                params: this.data
+            })
+            .then(function (res) {
+                const data = res / data;
+                const action = getInputChange(data);
+                dispatch(action)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,26 @@
 import {
-    createStore
+    createStore,
+    applyMiddleware,
+    compose
 } from 'redux'
+import thunk from 'redux-thunk';
 import reducer from './reducer'
+
+const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+);
+
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // 存放中间件的
+    enhancer
 )
 
 export default store;
