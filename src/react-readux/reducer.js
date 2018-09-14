2eb01deff5f722ff95f5f340e9e68f4497d671e8
heyushuo <1007561607@qq.com>
2018-09-14 15:34:10 +0800

    redux

---
+++ src/react-readux/reducer.js
@@ -0,0 +1,35 @@
+const defaultState = {
+    data: ['react', 'Vue'],
+    name: "何玉硕"
+}
+export const reducerName = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'ACTION_TYPE':
+            return
+        default:
+            return state
+    }
+}
+
+
+
+
+// <Provider store={store}>
+//     <App />
+// </Provider>
+
+//使用
+// const mapStateToProps = () => {
+//     return {
+
+//     }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+
+//     }
+// }
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(组件名字)
